41167c95874369aa57a3913946a7a78af24201dc
OzanVolkan <100294574+OzanVolkan@users.noreply.github.com>
2022-03-28 23:39:57 +0300

    Project Files

---
+++ ProjectStackDonuts/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
